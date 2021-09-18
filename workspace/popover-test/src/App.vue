026f7d01a8c1b344c89bdff8c7d80de371aad1c4
amplil <aikotobahaaburakatabura@gmail.com>
2021-09-18 20:03:27 +0900

    プロジェクト作成

---
+++ workspace/popover-test/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+
+  <!--
+  <div class="text-center my-3">
+    <b-button v-b-popover.hover.top="'I am popover directive content!'" title="Popover Title">
+      Hover Me
+    </b-button>
+
+    <b-button id="popover-target-1">
+      Hover Me
+    </b-button>
+    <b-popover target="popover-target-1" triggers="hover" placement="top">
+      <template #title>Popover Title</template>
+      I am popover <b>component</b> content!
+    </b-popover>
+  </div>
+  -->
+
+</template>
+
+<script>
+import HelloWorld from './components/popover.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ workspace/popover-test/src/components/popover.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="text-center my-3">
+    test
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--
+    <b-button v-b-popover.hover.top="'I am popover directive content!'" title="Popover Title">
+      Hover Me
+    </b-button>
+    -->
+
+    <b-button id="popover-target-1">
+      Hover Me
+    </b-button>
+    <b-popover target="popover-target-1" triggers="hover" placement="top">
+      <template #title>Popover Title</template>
+      I am popover <b>component</b> content!
+    </b-popover>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
